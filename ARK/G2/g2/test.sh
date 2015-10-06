45563382963763d6cec9fe23844591dc4ce64d4a
Mads Gram <mgmadsgram@gmail.com>
2015-10-06 17:48:40 +0100

    Test shell script

---
+++ ARK/G2/g2/test.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+set -e
+
+make clean
+make
+
+cd asm
+
+make clean
+make
+
+cd ..
+
+./sim default.cfg asm/sw-lw.elf
+./sim default.cfg asm/add.elf
+./sim default.cfg asm/beq-true-nopsled.elf
+./sim default.cfg asm/beq-false-nopsled.elf
+./sim default.cfg asm/j-nopsled.elf
+./sim default.cfg asm/jal-nopsled.elf
+./sim default.cfg asm/jal-jr-j-nopsled.elf
+
