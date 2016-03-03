18db377c4a406a6b09e173e4e3bda9a6988f58de
kazyka <lekroya@gmail.com>
2016-03-03 11:22:05 +0100

    Mangler test filer test 2, ellers fint

--- OSM/Assignments/Third_Assignment/queue/queue.c
+++ OSM/Assignments/Third_Assignment/queue/queue.c
@@ -9,6 +9,9 @@
 
 // All of the below functions assume that q is not NULL.
 
+pthread_mutex_t lock;
+pthread_cond_t cond;
+
 static inline size_t
 parent(size_t i) {
   return i / 2;
@@ -58,13 +61,13 @@ queue_heap_down(struct node *array, size_t count, size_t i) {
 int
 queue_init(struct queue *q) {
   /* FIXME: Make this function also initialize the pthread objects. */
-  pthread_mutex_init(*q);
   q->root = (struct node *)malloc(
     sizeof(struct node) * INIT_SIZE);
   q->next = q->root;
   q->count = 0;
   q->size = INIT_SIZE;
-
+  pthread_mutex_init(&lock, NULL);
+  pthread_cond_init(&cond, NULL);
   return 0;
 }
 
@@ -89,12 +92,16 @@ queue_grow(struct queue *q) {
 int
 queue_push(struct queue *q, int pri) {
   /* FIXME: Make this function thread-safe. */
-
+  pthread_mutex_lock(&lock);
   int retval;
 
   if (q->count >= q->size) {
     retval = queue_grow(q);
-    if (retval != 0) return retval;
+    if (retval != 0) 
+      {
+        pthread_mutex_unlock(&lock);
+        return retval;
+      }
   }
 
   q->next->pri = pri;
@@ -104,7 +111,8 @@ queue_push(struct queue *q, int pri) {
   q->count++;
 
 
-
+  pthread_mutex_unlock(&lock);
+  pthread_cond_signal(&cond);
   return 0;
 }
 
@@ -112,10 +120,12 @@ int
 queue_pop(struct queue *q, int *pri_ptr) {
   /* FIXME: Make this function thread-safe.  Also, if the queue is empty on pop,
      block until something is pushed. */
-
-  if (q->count == 0) {
-    return QUEUE_UNDERFLOW;
+  pthread_mutex_lock(&lock);
+  while (q->count == 0) 
+  {
+    pthread_cond_wait(&cond, &lock);
   }
+  pthread_mutex_unlock(&lock);
   *pri_ptr = q->root->pri;
 
   q->count--;
@@ -123,12 +133,12 @@ queue_pop(struct queue *q, int *pri_ptr) {
 
   exchange(q->root, q->next);
   queue_heap_down(q->root, q->count, 0);
-
   return 0;
 }
 
 int
 queue_destroy(struct queue *q) {
   free(q->root);
+  pthread_mutex_destroy(&lock);
   return 0;
 }
