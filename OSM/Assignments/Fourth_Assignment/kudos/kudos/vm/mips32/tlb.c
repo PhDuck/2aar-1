1959420ccb64c3cad0013f610f2516b1443a1cd3
Mads Gram <mgmadsgram@gmail.com>
2016-03-11 12:35:27 +0100

    Load and store done

--- OSM/Assignments/Fourth_Assignment/kudos/kudos/vm/mips32/tlb.c
+++ OSM/Assignments/Fourth_Assignment/kudos/kudos/vm/mips32/tlb.c
@@ -4,22 +4,56 @@
 
 #include "kernel/panic.h"
 #include "kernel/assert.h"
+#include "kernel/thread.h"
+#include "proc/process.h"
 #include <tlb.h>
 #include <pagetable.h>
 
-void tlb_modified_exception(void)
+int find_matching_entry(pagetable_t* pagetable, int* entry_index)
+{
+  tlb_exception_state_t* state;
+  _tlb_get_exception_state(&state);
+
+  for (int i = 0; i < PAGETABLE_ENTRIES; ++i)
+  {
+    if (state.badvpn2 == pagetable->entries[i].VPN2)
+    {
+      entry_index* = i;
+      return 0;
+    }
+  }
+  return NO_PAGE_ENTRY_FOUND;
+}
+
+void tlb_modified_exception()
 {
   KERNEL_PANIC("Unhandled TLB modified exception");
 }
 
 void tlb_load_exception(void)
 {
-  KERNEL_PANIC("Unhandled TLB load exception");
+  pagetable_t pagetable = thread_get_current_thread() -> pagetable;
+
+  if (pagetable.entries == NULL) {
+    KERNEL_PANIC("TLBL: Kernel Mode, we must panic.\n");
+  }
+
+  int search_result, entry_index;
+
+  search_result = find_matching_entry(pagetable, entry_index);
+
+  if (search_result == 0) {
+    tlb_entry_t entry = pagetable->entries[entry_index];
+    _tlb_write_random(&entry);
+  } else {
+    proccess_exit(search_result);
+  }
+
 }
 
 void tlb_store_exception(void)
 {
-  KERNEL_PANIC("Unhandled TLB store exception");
+  tlb_load_exception();
 }
 
 /**
